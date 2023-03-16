d75f843fcf144defc4b961ffb4e52fe3951048eb
sahil <guptasahil198@gmail.com>
2023-03-16 17:17:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import MovieCard from './MovieCard';
+import searchIcon from './search.svg';
+
+const API_URL = 'http://www.omdbapi.com?apikey=987bb2e2';
 
 function App() {
+  // api key: 987bb2e2
+
+  const [movies, setMovies] = useState([]);
+
+  const [searchTerm, setSearchTerm] = useState();
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    setMovies(data.Search);
+  }
+
+  // in starting website will load movies according to this useEffect
+  useEffect(() => {
+    searchMovies('harry potter');
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>MovieOne</h1>
+
+      <div className="search ">
+        <input type="text" placeholder='search for movies' value={searchTerm}
+          onChange={(event) => { setSearchTerm(event.target.value) }}
+        />
+        <img src={searchIcon} alt="search" onClick={() => { searchMovies(searchTerm) }}
+        />
+      </div>
+
+      {
+        movies?.length > 0
+          ? (<div className="container">
+            {movies.map((movie) => (
+              <MovieCard movie1={movie} />
+            ))}
+          </div>
+          ) : (
+            <div className='empty'>
+              <h2>no movies found</h2>
+            </div>
+          )
+      }
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/MovieCard.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function MovieCard(props) {
+  return (
+    <div>
+      <div className="movie">
+        <div>
+          <p>{props.movie1.Year}</p>
+        </div>
+
+        <div>
+          <img src={props.movie1.Poster !== 'N/A' ? props.movie1.Poster : 'https://via.placeholder.com/400'} alt={props.movie1.Title} />
+        </div>
+
+        <div>
+          <span>{props.movie1.Type}</span>
+          <h3>{props.movie1.Title}</h3>
+        </div>
+      </div>
+    </div>
+  )
+}
